de54f0ecc27831f2a6e8677f71f196840dd49df2
Bryan Higgins <60978890+bryhig@users.noreply.github.com>
2021-03-02 16:55:49 +0000

    Delete controllers/create_go_forward directory

--- controllers/create_go_forward/build/release/create_go_forward.d
+++
@@ -1,8 +0,0 @@
-build/release/create_go_forward.o: create_go_forward.cpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Program\ Files/Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/cpp/webots/TouchSensor.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Receiver.hpp
