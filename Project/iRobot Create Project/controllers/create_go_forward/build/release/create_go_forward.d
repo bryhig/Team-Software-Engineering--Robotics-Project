a8d5ce1f07aace7ed1b3766771c3c315d476fedc
Bryan Higgins <60978890+bryhig@users.noreply.github.com>
2021-03-02 16:56:51 +0000

    Delete Project/iRobot Create Project/controllers/create_go_forward directory

--- Project/iRobot Create Project/controllers/create_go_forward/build/release/create_go_forward.d
+++
@@ -1,6 +0,0 @@
-build/release/create_go_forward.o: create_go_forward.cpp \
- C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
- C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Users/bryhi/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
- C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp
