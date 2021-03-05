c73d8733f8bf34be47797ab3cab16a0501b235eb
AlmightyGizzard <59885563+AlmightyGizzard@users.noreply.github.com>
2021-03-05 00:55:05 +0000

    Turn function preliminary
    The basic outline of the code that turns the robot is done - however testing is difficult with the current code structure - once that's set up can fully test + implement angle turning.

--- Project/iRobot Create Project/controllers/vacuum_cleaning_robot_controller/vacuum_cleaning_robot_controller.cpp
+++ Project/iRobot Create Project/controllers/vacuum_cleaning_robot_controller/vacuum_cleaning_robot_controller.cpp
@@ -12,9 +12,16 @@
 #include <webots/Motor.hpp>
 
 #define TIME_STEP 64
-#define MAX_SPEED 6.28
+#define MAX_SPEED 6.0
+#define HALF_SPEED 3.0
+#define WHEEL_RADIUS 0.031
+#define AXLE_LENGTH 0.271756
 
 using namespace webots;
+
+
+
+
 
 //Obstacle detection functions that check to see if sensors are at threshold value. BH
 //collision and cliff functions work for all directions. Change function inputs for different directions. BH
@@ -30,6 +37,53 @@ bool cliff(double dsValue1, double dsValue2)
  return cliff;
 }
 
+/*
+// Not in use now, will be usable after code is restructured.
+
+void go_forward(){
+  leftMotor->setVelocity(MAX_SPEED);
+  rightMotor->setVelocity(MAX_SPEED);
+}
+
+void go_backward(){
+  leftMotor->setVelocity(-MAX_SPEED);
+  rightMotor->setVelocity(-MAX_SPEED);
+}
+
+void stop(){
+  leftMotor->setVelocity(0.0);
+  rightMotor->setVelocity(0.0);
+}
+
+// Partially pulled from the webots robot, in order to test this
+void turn(double angle){
+  // Grab the current positions of the wheels (at start of turning) to apply when working out how far we've turned. DC
+  double leftOffset = ps[0]->getValue();
+  double rightOffset = ps[1]->getValue();
+  
+  // If the angle is negative, turn left, else turn right. DC
+  double direction = (angle < 0) ? -1.0 : 1.0;
+  
+  // Set the motors going - if left, set the left motor forward and right backward,
+  // otherwise do the opposite. DC
+  leftMotor->setVelocity(direction * HALF_SPEED);
+  rightMotor->setVelocity(-direction * HALF_SPEED);
+  
+  //until this variable == the angle entered, keep rotating
+  double orientation;
+  do{
+    // Grab the current positions of the wheels, DC
+    double l = ps[0]->getValue();
+    double r = ps[1]->getValue();
+    // Work out the distance we move based on the radius of the wheels, DC
+    double dl = l * WHEEL_RADIUS;
+    double dr = r * WHEEL_RADIUS;
+    // work out the current orientation by shifting the circle by the distance moved. DC
+    // Circle diameter = axle length. DC
+    orientation = direction * (dl-dr) * AXLE_LENGTH; 
+  } while(orientation < direction * angle);
+}
+*/
 
 int main(int argc, char **argv) {
   
@@ -78,6 +132,8 @@ int main(int argc, char **argv) {
   
   leftMotor->setVelocity(0.0);
   rightMotor->setVelocity(0.0);
+  
+  
 
   // Main loop:
   //Avoidance algorithms go in here. BH
@@ -103,6 +159,12 @@ int main(int argc, char **argv) {
    {
     psValues[i] = ps[i]->getValue();
    }
+  
+   // Left the actual turn code here - once the function is tested we can specify what angle etc. DC
+   leftMotor->setVelocity(1 * HALF_SPEED);
+   rightMotor->setVelocity(-1 * HALF_SPEED);
+   // Read the position sensors 
+   printf("%lf on the left, and %lf on the right\n", ps[0]->getValue(), ps[1]->getValue());
    
    //Obstacle detection functions called. Input parameters specify direction for collision and cliff. BH
    bool isLeftCollision = collision(tsValues[0]);
@@ -112,9 +174,12 @@ int main(int argc, char **argv) {
    bool isCliffRight = cliff(dsValues[3], dsValues[2]);
    bool isCliffFront = cliff(dsValues[1], dsValues[2]);
   
-  //Flush IR receiver. MUST STAY AT END OF LOOP. BH.
-  while (r->getQueueLength() > 0){r->nextPacket();}
+   //Flush IR receiver. MUST STAY AT END OF LOOP. BH.
+   while (r->getQueueLength() > 0){r->nextPacket();}
+   
+   
+  }
+  // Moved these here as they were killing the loop after 1 pass. DC
   delete robot;
   return 0;
-  }
  }
