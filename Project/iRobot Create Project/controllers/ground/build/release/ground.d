6ea6ee5a51dea1f65c17f4e309b66d3f1c882b21
Bryan Higgins <60978890+bryhig@users.noreply.github.com>
2021-03-24 15:11:28 +0000

    Dust texture added to plane, however robot does not erase dust texture.
    The robot does not erase the dust texture after moving over dust for an unknown reason. Using an existing sample supervisor controller from Webots.

---
+++ Project/iRobot Create Project/controllers/ground/build/release/ground.d
@@ -0,0 +1,6 @@
+build/release/ground.o: ground.c \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/c/webots/display.h \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/c/webots/supervisor.h
