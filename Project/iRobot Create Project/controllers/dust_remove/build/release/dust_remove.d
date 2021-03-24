58a47c22b4444140882a4ed7a4dcd400f47322b7
Bryan Higgins <60978890+bryhig@users.noreply.github.com>
2021-03-24 17:29:27 +0000

    Fully functional supervisor controller to remove dust written in C++
    A fully functional supervisor controller that makes the robot appear as if it is removing dust from the floor. This shows where the robot has been and cleaned. This is no longer using the Webots sample code, and is now using a new implementation written in C++ by myself.

---
+++ Project/iRobot Create Project/controllers/dust_remove/build/release/dust_remove.d
@@ -0,0 +1,9 @@
+build/release/dust_remove.o: dust_remove.cpp \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/Supervisor.hpp \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/bryhi/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/Node.hpp \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/Field.hpp \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/Display.hpp \
+ C:\Users\bryhi\AppData\Local\Programs\Webots/include/controller/cpp/webots/ImageRef.hpp
